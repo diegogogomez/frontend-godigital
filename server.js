ba69181f1a94b6fd479dfa01af420af1674fa780
Diego Fernando Gómez <d.fernandogomez94@gmail.com>
2021-08-23 00:06:28 -0500

    despliegue v3

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/godigital'));
+app.use(express.static(__dirname + '/dist/go-digital'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/godigital/index.html'));});
+    res.sendFile(
+        path.join(__dirname+'/dist/go-digital/index.html')
+        );
+});
 app.listen(process.env.PORT || 8080);
